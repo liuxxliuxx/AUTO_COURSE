ecde98420e8cd0bb21048d7a6f4a172a107de535
liuxxliuxx <liuxxliuxx@yeah.net>
2026-09-09 22:21:43 +0800

    删除神秘临时文件

--- tmp_chk.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import io
-
-content = io.open(r'D:/Projects/AUTO_COURSE/src/bot/map_course.py', encoding='utf-8').read()
-print('当前文件行数:', len(content.splitlines()))
-print('包含 _get_video_info:', '_get_video_info' in content)
-print('包含 _play_resource:', '_play_resource' in content)
-
